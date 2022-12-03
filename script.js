4380f84ccbd74f0dc5ba88a970526c3b0eea1bb3
JordanLindley <jlindley94@gmail.com>
2022-12-03 12:26:50 -0500

    add operator function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// assign operators to variables to capture text in DOM through functions
+let firstOperand = '';
+let secondOperan = '';
+let currentOperand = null;
+let shouldResetDisply = false;
+
 // pull elements from the DOM, store them in variables
 const numbers = document.querySelectorAll('.number');
 const decimal = document.querySelector('.decimal');
@@ -8,18 +14,18 @@ const negative = document.querySelector('.negative');
 const currentDisplay = document.querySelector('.current');
 const historyDisplay = document.querySelector('.history');
 // Operators:
-const add = document.querySelector('.add');
-const subtract = document.querySelector('.subtract');
-const multiply = document.querySelector('.multiply');
-const divide = document.querySelector('.divide');
+const addition = document.querySelector('.add');
+const subtraction = document.querySelector('.subtract');
+const multiplication = document.querySelector('.multiply');
+const division = document.querySelector('.divide');
 const equals = document.querySelector('.equals');
 
 // add event listeners for each button and assign to variables
   // numbers (listen for click or keydown to corresponding textcontent and use the text content of the element)
 numbers.forEach(number => {
   number.addEventListener('click', () => {
-    if (currentDisplay.textContent == '') {
-    currentDisplay.textContent = number.textContent;
+    if (currentDisplay.textContent == ``) {
+    currentDisplay.innterText = `${number.innerText}`;
     }
     else {
       
@@ -27,7 +33,14 @@ numbers.forEach(number => {
   });
 })
 
-window.addEventListener('keypress', e => {
+equals.addEventListener('click', operate);
+equals.addEventListener('keypress', e => {
+  if (e.key === 'Enter') {
+    operate;
+  }
+})
+
+window.addEventListener('keydown', e => {
   let keycode = e.which || e.keyCode;
   let valueEntered = String.fromCharCode(keycode);
   console.log(valueEntered);
@@ -35,20 +48,36 @@ window.addEventListener('keypress', e => {
   // operators (listen for click or keydown for specific operator on keypad, different variables for each operator)
   // a part of each event listener should be to append the text content of the button to the display div
 
-// add typed or clicked number to display div
+// // add typed or clicked number to display div
 function typeNumber(key) { 
-  let liveDisplay;
+
   // currentDisplay.innerText = `${key.innerText}`;
 }
 
 // Addition function
   // (for each function below) create if statement that checks if there is already text in the display and use that as num1 if operator button pressed again before user types or presses another button.
+const add = (a, b) => {
+  return a + b;
+}
 
 // Multiply function
+const multiply = (a, b) => {
+  return a * b;
+}
 
 // Divide function
+const divide = (a, b) => {
+  return a / b;
+}
 
 // Subtract funtion
+const subtract = (a, b) => {
+  return a - b;
+}
 
+// operate function, run all operators
+function operate(math, number1, number2) {
+  
+}
 // Display logic
   // print last-pressed button as history (need new div?)
